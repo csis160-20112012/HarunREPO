619b8342e603c5744655d0ec1865bb83dd2b89cc
Harun <harundulic@gmail.com>
2012-05-16 14:04:32 +0200

    This is altered javadoc comment.

--- src/gitPaket/GlavnaKlasa.java
+++ src/gitPaket/GlavnaKlasa.java
@@ -1,8 +1,20 @@
 package gitPaket;
 
+/**
+ * This is the main class
+ * @author harun.djulic
+ * @version 1.0.1
+ * 
+ *
+ *
+ */
 public class GlavnaKlasa {
 
-	
+	/**
+	 * This is the method for printing a simple line of text.
+	 * It uses System.out.println command
+	 * @param args
+	 */
 	
 	public static void main(String[] args) {
 		
