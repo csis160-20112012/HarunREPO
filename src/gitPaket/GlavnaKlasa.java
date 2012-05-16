7c81d606f5d4a14c733281bea52a9e0f8989adb8
Harun <harundulic@gmail.com>
2012-05-16 13:37:31 +0200

    new commit of the change

--- src/gitPaket/GlavnaKlasa.java
+++ src/gitPaket/GlavnaKlasa.java
@@ -6,7 +6,7 @@ public class GlavnaKlasa {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Test text for GitHub and Eclipse");
+		System.out.println("Test text for GitHub and Eclipse, test for change");
 		
 
 	}
